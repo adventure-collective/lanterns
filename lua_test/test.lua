10f6631ff17465f64f6675b341cf6ef7596dd35a
Ben Foxall <benfoxall@gmail.com>
2017-04-25 10:42:31 +0100

    switch to using busted

--- lua_test/test.lua
+++ lua_test/test.lua
@@ -1,48 +1,49 @@
--- run from from main directory `lua lua_test/test.lua`
-dofile('lua_test/minctest.lua')
+-- requires busted
+-- busted lua_test/test.lua
+
 dofile('lua/handlers.lua')
 
+describe("hex to rbg", function()
 
-lrun("basic hex", function()
+  it("should handle basic hex", function()
 
-  local r,g,b = hextorgb('123456')
-  lequal(r, 18)
-  lequal(g, 52)
-  lequal(b, 86)
+    local r,g,b = hextorgb('123456')
+    assert.are.same(r, 18)
+    assert.are.same(g, 52)
+    assert.are.same(b, 86)
 
-  local r,g,b = hextorgb('ffffff')
-  lequal(r, 255)
-  lequal(g, 255)
-  lequal(b, 255)
+    local r,g,b = hextorgb('ffffff')
+    assert.are.same(r, 255)
+    assert.are.same(g, 255)
+    assert.are.same(b, 255)
 
-  local r,g,b = hextorgb('000000')
-  lequal(r, 0)
-  lequal(g, 0)
-  lequal(b, 0)
+    local r,g,b = hextorgb('000000')
+    assert.are.same(r, 0)
+    assert.are.same(g, 0)
+    assert.are.same(b, 0)
+
+  end)
+
+
+  it("should handle different hex", function()
+
+    local r,g,b = hextorgb('12345612345')
+    assert.are.same(r, 18)
+    assert.are.same(g, 52)
+    assert.are.same(b, 86)
+
+    -- not sure about values here, but mainly, it shouldn't crash
+    local r,g,b = hextorgb('this is not a hex')
+    assert.are.same(r, 0)
+    assert.are.same(g, 0)
+    assert.are.same(b, 0)
+
+    -- or here
+    local r,g,b = hextorgb('0')
+    assert.are.same(r, 0)
+    assert.are.same(g, 0)
+    assert.are.same(b, 0)
+
+  end)
 
 end)
-
-
-lrun("different hex", function()
-
-  local r,g,b = hextorgb('12345612345')
-  lequal(r, 18)
-  lequal(g, 52)
-  lequal(b, 86)
-
-  -- not sure about values here, but mainly, it shouldn't crash
-  local r,g,b = hextorgb('this is not a hex')
-  lequal(r, 0)
-  lequal(g, 0)
-  lequal(b, 0)
-
-  -- or here
-  local r,g,b = hextorgb('0')
-  lequal(r, 0)
-  lequal(g, 0)
-  lequal(b, 0)
-
-end)
-
-
-return lresults()
