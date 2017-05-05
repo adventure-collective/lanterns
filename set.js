1091454d33abb74aa2261654d4587311eefe2ac4
Ben Foxall <benfoxall@gmail.com>
2017-05-05 15:15:42 +0100

    Clean up set script

--- set.js
+++ set.js
@@ -1,5 +1,5 @@
 const brightness = 10 // 0 -> 127
-const N = 16
+const N = 16 // Number of Pixels
 
 
 // use the first bunch listed in network.json
@@ -12,23 +12,19 @@ console.log(`Using bunch:${bunch_id} (${bunch.address}:${bunch.port})`)
 const dgram = require('dgram')
 const client = dgram.createSocket('udp4')
 
-const inter = setInterval(() => {
+setInterval(() => {
 
   const now = Date.now()
 
   var colours = Array.from({length: N})
     .map((v, i ) => {
-      const colour = String.fromCharCode(
+      return String.fromCharCode(
         (Math.sin(((now + (i * 80))/100) + 0) + 1) * brightness,
         (Math.sin(((now + (i * 80))/100) + 2) + 1) * brightness,
         (Math.sin(((now + (i * 80))/100) + 4) + 1) * brightness
       )
+    }).join('')
 
-      return colour
-
-    })
-
-
-  client.send(Buffer.from(`SET ${colours.join('')}`), bunch.port, bunch.address);
+  client.send(Buffer.from(`SET ${colours}`), bunch.port, bunch.address);
 
 }, 50)
