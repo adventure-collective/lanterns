59032c9f2e4406031f564e3e60004a61f6305dad
Ben Foxall <benfoxall@gmail.com>
2017-05-06 14:03:39 +0100

    interpolation

--- frontend/Lanterns.js
+++ frontend/Lanterns.js
@@ -1,4 +1,3 @@
-
 class Lanterns {
   constructor (config) {
     this.config = config
@@ -17,9 +16,34 @@ class Lanterns {
           })
       })
 
+    // clean up
     this._raw.forEach(item => {
       delete item.count
     })
+
+    // interpolate positions
+    var last = {x: 0, y: 0, z: 0}
+    var next = {x: 0, y: 0, z: 0}
+    for (var i = 0; i < this._raw.length; i++) {
+      if(typeof(this._raw[i].x) == 'undefined') {
+        for(var j = i; j < this._raw.length; j++) {
+          if(typeof(this._raw[j].x) != 'undefined') {
+            next.x = this._raw[j].x
+            next.y = this._raw[j].y
+            next.z = this._raw[j].z
+            break
+          }
+        }
+        for (var k = i; k < j; k++) {
+          const off = (k - i + 1) / (j - i + 1)
+          this._raw[k].x = last.x + (next.x - last.x) * off
+          this._raw[k].y = last.y + (next.y - last.y) * off
+          this._raw[k].z = last.z + (next.z - last.z) * off
+        }
+      }
+    }
+
+
   }
 
   raw() {
--- frontend/test.js
+++ frontend/test.js
@@ -56,3 +56,26 @@ test('Count', t => {
   ])
 
 })
+
+
+test('Interpolation', t => {
+  t.plan(1)
+
+  const lanterns = new Lanterns({
+    AA: [
+      {x: 0, y: 0, z: 0},
+      {},{},
+      {x: 3, y: 3, z: 3}
+    ]
+  })
+
+  const raw = lanterns.raw()
+
+  t.deepEqual(raw, [
+    {x: 0, y: 0, z: 0},
+    {x: 1, y: 1, z: 1},
+    {x: 2, y: 2, z: 2},
+    {x: 3, y: 3, z: 3}
+  ])
+
+})
